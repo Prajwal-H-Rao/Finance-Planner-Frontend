03afeeade8b4c6120c1ab62e1bfcfaca1c7fbf8f
Prajwal-H-Rao <prajwalhr130@gmail.com>
2025-10-26 20:17:20 +0530

    added protected routes

--- src/constants/Routes.ts
+++ src/constants/Routes.ts
@@ -1,15 +1,21 @@
+import Dasboard from "../pages/Dasboard";
 import Login from "../pages/Login";
 import Signup from "../pages/Signup";
 
 export const Routes = [
   {
-    path: "login",
+    path: "/login",
     component: Login,
     protected: false,
   },
   {
-    path: "signup",
+    path: "/signup",
     component: Signup,
     protected: false,
+  },
+  {
+    path: "/",
+    component: Dasboard,
+    protected: true,
   },
 ];
